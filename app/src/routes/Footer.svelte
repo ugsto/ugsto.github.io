6257b6a0d395ff47e4fdec6cd16336e96e360b37
André (ugsto) Bortoli <andre.ugsto@proton.me>
2023-12-21 12:30:43 -0300

    fix: include inline-block to aboid icon-text break

--- app/src/routes/Footer.svelte
+++ app/src/routes/Footer.svelte
@@ -12,17 +12,18 @@
 				<a
 					href="https://www.linkedin.com/in/andre-augusto-bortoli"
 					target="_blank"
+					class="inline-block"
 					aria-label="LinkedIn"
 				>
 					<LinkedInIcon class="inline" />
 					<span class="underline">LinkedIn</span>
 				</a>
 				or
-				<a href="https://github.com/ugsto" target="_blank" aria-label="GitHub">
+				<a href="https://github.com/ugsto" target="_blank" class="inline-block" aria-label="GitHub">
 					<GitHubIcon class="inline" />
 					<span class="underline">GitHub</span>
 				</a>. For collaborations or inquiries, feel free to
-				<a href="mailto:andre@bortoli.phd" aria-label="Email">
+				<a href="mailto:andre@bortoli.phd" class="inline-block" aria-label="Email">
 					<EmailIcon class="inline" />
 					<span class="underline">reach out</span>
 				</a>.
