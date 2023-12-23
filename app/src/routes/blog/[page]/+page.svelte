c2fe416be845cb1c3a10eb03bac086a408b0308f
André (ugsto) Bortoli <andre.ugsto@proton.me>
2023-12-23 19:44:20 -0300

    feat: include post date

--- app/src/routes/blog/[page]/+page.svelte
+++ app/src/routes/blog/[page]/+page.svelte
@@ -10,21 +10,26 @@
 
 <div class="flex flex-col flex-grow justify-between">
 	<div class="flex flex-col sm:flex-row px-8 py-4">
-		<section id="posts" class="w-3/4">
+		<section id="posts" class="w-3/4 pr-16">
 			<Title>Posts</Title>
-			{#each data.posts as post}
-				<div class="mb-4">
-					<Subtitle><Link href={`/blog/posts/${post.id}`}>{post.title}</Link></Subtitle>
-					<p>{post.description}</p>
-					<div class="flex flex-wrap gap-2 mt-2">
-						{#each post.tags as tag}
-							<Tag {tag} />
-						{/each}
+			<div class="flex flex-col gap-12">
+				{#each data.posts as post}
+					<div>
+						<Subtitle><Link href={`/blog/posts/${post.id}`}>{post.title}</Link></Subtitle>
+						<p class="text-justify">{post.description}</p>
+						<div class="flex justify-between">
+							<div class="flex flex-wrap gap-2 mt-2">
+								{#each post.tags as tag}
+									<Tag {tag} />
+								{/each}
+							</div>
+							<span class="text-sm text-slate-11">{new Date(post.date).toLocaleDateString()}</span>
+						</div>
 					</div>
-				</div>
-			{/each}
+				{/each}
+			</div>
 		</section>
-		<aside class="w-full sm:w-1/4">
+		<aside class="block ml-auto w-full sm:w-1/4 max-w-fit">
 			<Title>Tags</Title>
 			<div class="flex flex-wrap gap-2">
 				{#each data.tags as tag}
