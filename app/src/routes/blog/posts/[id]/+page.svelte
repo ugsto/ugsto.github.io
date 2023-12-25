aebe3672ec9d145b07b767767978e941201a091e
André (ugsto) Bortoli <andre.ugsto@proton.me>
2023-12-25 01:39:34 -0300

    fix: responsivity

--- app/src/routes/blog/posts/[id]/+page.svelte
+++ app/src/routes/blog/posts/[id]/+page.svelte
@@ -9,20 +9,20 @@
 	const { author, date, tags, content, readingTime } = data;
 </script>
 
-<div class=" bg-slate-2 dark:bg-slate-d2">
-	<div class="flex flex-col w-fit md:flex-row md:items-start mx-auto">
-		<aside class="mt-4 px-8 py-4">
-			<div class="flex flex-col gap-4">
-				<PostAuthor {author} {date} {readingTime} />
-				<div class="flex flex-wrap gap-2">
-					{#each tags as tag}
-						<Tag {tag} />
-					{/each}
-				</div>
+<div class="bg-slate-2 dark:bg-slate-d2 flex-grow pt-4">
+	<aside class="block px-8 py-4 mx-auto w-fit">
+		<div class="flex flex-col gap-4">
+			<PostAuthor {author} {date} {readingTime} />
+			<div class="flex flex-wrap gap-2">
+				{#each tags as tag}
+					<Tag {tag} />
+				{/each}
 			</div>
-		</aside>
-		<article id="post" class="px-8 py-4 prose lg:prose-xl dark:prose-invert">
+		</div>
+	</aside>
+	<main>
+		<article id="post" class="px-8 py-4 prose md:prose-xl dark:prose-invert">
 			<Markdown {content} />
 		</article>
-	</div>
+	</main>
 </div>
