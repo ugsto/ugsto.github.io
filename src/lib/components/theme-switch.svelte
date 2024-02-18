34c5aea18fbaee7bedec60eeea1bf5c11d1d72d9
André Bortoli <andre@bortoli.phd>
2024-02-18 14:42:19 -0300

    feat: include dark mode

---
+++ src/lib/components/theme-switch.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Icon from '@iconify/svelte';
+
+	export let theme = 'light';
+
+	function toggleTheme() {
+		if (theme === 'light') {
+			theme = 'dark';
+			document.body.classList.add('dark');
+		} else {
+			theme = 'light';
+			document.body.classList.remove('dark');
+		}
+	}
+</script>
+
+<button
+	on:click={toggleTheme}
+	aria-label="Toggle theme"
+	class="rounded p-1 bg-slate-6 hover:bg-slate-7 dark:bg-slate-d6 dark:hover:bg-slate-d7"
+>
+	<Icon
+		icon="mdi:weather-sunny"
+		aria-current={theme === 'light' ? 'page' : undefined}
+		class="block dark:hidden text-xl text-amber-11"
+	/>
+	<Icon
+		icon="mdi:weather-night"
+		aria-current={theme === 'dark' ? 'page' : undefined}
+		class="hidden dark:block text-xl text-sage-d11"
+	/>
+</button>
--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { t } from '$lib/translations';
 	import { onMount } from 'svelte';
+	import ThemeSwitch from '$lib/components/theme-switch.svelte';
 
 	let activeSection = '';
 
@@ -32,47 +33,52 @@
 	});
 </script>
 
-<header
-	class="container sm:sticky flex flex-col sm:flex-row justify-between items-center top-0 gap-4 mx-auto p-2 bg-slate-2 rounded-b-xl"
->
-	<a class="flex items-center gap-4 w-fit" href="/">
-		<img src="/favicon.svg" alt="logo" class="h-16 w-16" /><span
-			class="font-mono font-bold text-2xl select-none">André Bortoli</span
-		>
-	</a>
+<header class="bg-slate-2 dark:bg-slate-d2">
+	<div class="container flex flex-col sm:flex-row justify-between items-center mx-auto py-1">
+		<a class="flex items-center rounded gap-2 w-fit p-1 bg-slate-a6 dark:bg-slate-da6" href="/">
+			<img src="/favicon.svg" alt="logo" class="w-16" />
+			<span class="font-mono font-bold text-2xl text-slate-12 dark:text-slate-d12 select-none">
+				André Bortoli
+			</span>
+		</a>
 
-	<nav aria-label="Main navigation">
-		<ul class="flex gap-4">
-			<li>
-				<a
-					class="inline-block px-3 py-2 rounded hover:bg-slate-4 bg-slate-3 text-slate-12"
-					aria-current={activeSection === 'about' ? 'page' : undefined}
-					href="#about">{$t('home.navigation.about')}</a
-				>
-			</li>
-			<li>
-				<a
-					class="inline-block px-3 py-2 rounded hover:bg-slate-4 bg-slate-3 text-slate-12"
-					aria-current={activeSection === 'projects' ? 'page' : undefined}
-					href="#projects">{$t('home.navigation.projects')}</a
-				>
-			</li>
-			<li>
-				<a
-					class="inline-block px-3 py-2 rounded hover:bg-slate-4 bg-slate-3 text-slate-12"
-					aria-current={activeSection === 'blog' ? 'page' : undefined}
-					href="#blog">{$t('home.navigation.blog')}</a
-				>
-			</li>
-			<li>
-				<a
-					class="inline-block px-3 py-2 rounded hover:bg-slate-4 bg-slate-3 text-slate-12"
-					aria-current={activeSection === 'contact' ? 'page' : undefined}
-					href="#contact">{$t('home.navigation.contact')}</a
-				>
-			</li>
-		</ul>
-	</nav>
+		<nav aria-label="Main navigation">
+			<ul class="flex gap-4">
+				<li>
+					<a
+						class="inline-block px-3 py-2 rounded bg-slate-a6 hover:bg-slate-a7 dark:bg-slate-d6 dark:hover:bg-slate-d7 text-slate-12 dark:text-slate-d12"
+						aria-current={activeSection === 'about' ? 'page' : undefined}
+						href="#about">{$t('home.navigation.about')}</a
+					>
+				</li>
+				<li>
+					<a
+						class="inline-block px-3 py-2 rounded bg-slate-a6 hover:bg-slate-a7 dark:bg-slate-d6 dark:hover:bg-slate-d7 text-slate-12 dark:text-slate-d12"
+						aria-current={activeSection === 'projects' ? 'page' : undefined}
+						href="#projects">{$t('home.navigation.projects')}</a
+					>
+				</li>
+				<li>
+					<a
+						class="inline-block px-3 py-2 rounded bg-slate-a6 hover:bg-slate-a7 dark:bg-slate-d6 dark:hover:bg-slate-d7 text-slate-12 dark:text-slate-d12"
+						aria-current={activeSection === 'blog' ? 'page' : undefined}
+						href="#blog">{$t('home.navigation.blog')}</a
+					>
+				</li>
+				<li>
+					<a
+						class="inline-block px-3 py-2 rounded bg-slate-a6 hover:bg-slate-a7 dark:bg-slate-d6 dark:hover:bg-slate-d7 text-slate-12 dark:text-slate-d12"
+						aria-current={activeSection === 'contact' ? 'page' : undefined}
+						href="#contact">{$t('home.navigation.contact')}</a
+					>
+				</li>
+			</ul>
+		</nav>
+
+		<div>
+			<ThemeSwitch />
+		</div>
+	</div>
 </header>
 
 <section id="about" class="bg-slate-8 w-screen h-screen"></section>
