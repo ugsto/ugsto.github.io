8e90a8cb39081b591e0da33fc0c7384ac14cc23c
André Bortoli <andre@bortoli.phd>
2024-03-01 00:54:37 -0300

    refactor: abstract away starry background component

---
+++ src/lib/components/starry-background.svelte
@@ -0,0 +1,22 @@
+<script>
+	import './stars.css';
+	import init, { randomStars, plotStars } from '$lib/wasm/starry_background/pkg/starry_background';
+	import { onMount } from 'svelte';
+
+	/**
+	 * @type {string}
+	 */
+	export let id;
+
+	onMount(() => {
+		init().then(() => {
+			plotStars(randomStars(50), document.getElementById('sky'));
+		});
+	});
+</script>
+
+<div
+	{id}
+	class="absolute top-0 left-0 w-full h-full overflow-hidden -z-10 bg-sky-9 dark:bg-sky-d1"
+	aria-hidden="true"
+/>
--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -1,21 +1,13 @@
 <script>
-	import './star.css';
-	import init, { randomStars, plotStars } from '$lib/wasm/starry_background/pkg/starry_background';
 	import { t } from '$lib/translations';
 	import ThemeSwitch from '$lib/components/theme-switch.svelte';
 	import { locale } from '$lib/translations';
 	import LanguageSwitch from '$lib/components/language-switch.svelte';
-	import { onMount } from 'svelte';
 	import Carousel from '$lib/components/carousel.svelte';
 	import LinkedinIcon from 'virtual:icons/mdi/linkedin';
 	import GithubIcon from 'virtual:icons/mdi/github';
 	import PhoneIcon from 'virtual:icons/mdi/phone';
-
-	onMount(() => {
-		init().then(() => {
-			plotStars(randomStars(50), document.getElementById('sky'));
-		});
-	});
+	import StarryBackground from '$lib/components/starry-background.svelte';
 </script>
 
 <header class="bg-iris-2 dark:bg-iris-d2 shadow-xl">
@@ -87,11 +79,7 @@
 			</p>
 		</div>
 	</div>
-	<div
-		id="sky"
-		class="absolute top-0 left-0 w-full h-full overflow-hidden -z-10 bg-sky-9 dark:bg-sky-d1"
-		aria-hidden="true"
-	/>
+	<StarryBackground id="sky" />
 </section>
 
 <section
